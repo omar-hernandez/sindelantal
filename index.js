2a5443979af5020fe5669a1c1145f0fca92e1faf
omar <erickurtuzuastegui@MacBook-Pro-de-Ofiadmin-4.local>
2018-08-06 21:22:33 -0500

    modificando index para subir a la red

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from 'express'
+import parser from 'body-parser'
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
+const PORT = process.env.PORT || 3003
+const mongoURI = porcess.env.MONGODB_URI || "mongodb://principalnegra:cintanegr4@ds113942.mlab.com:13942/sindelantal"
+
+
+mongoose.connect(mongoURI,{useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error',()=>console.log('Error al conectar a la BD'))
+    .once('open',()=>console.log('Conectado a la BD'))
+
+app.use(parser.json());
+app.use(cors());
 
 app.get('/', (req,res)=>{
-    res.send('Server on 2')
+    res.send('Server on')
 });
 
+app.listen(PORT,()=>{
+    console.log("server on" + PORT)
+})
 
-
-
-app.listen(3003, ()=> console.log('server on 3003'));
